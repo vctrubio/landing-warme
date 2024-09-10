736ea2756cb99b91d37d68429ad9160240ee4ba2
vctrubio <vctrubio@gmail.com>
2024-09-10 15:11:17 +0200

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
 import '../css/navbar.css'
 
 
-const navbar = () => {
-    return ( <div className='nav-bar'>
-        
-    
-    </div> );
+const NavBar = () => {
+    return (
+        <div className='nav-bar'>
+            <div className='home'>Home</div>
+            <div className='desc'>Description</div>
+            <div className='calc'>Calculator</div>
+            <div className='contact'>Contact</div>
+        </div>
+    );
 }
  
 export default navbar;
