7a80ec56a60409862ff843d6a248fcc0f8555fc1
vctrubio <vctrubio@gmail.com>
2024-09-10 15:32:07 +0200

    navbar positon 0

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import '../css/navbar.css';
 
 const NavBar = () => {
-    return (
-        <div className='nav-bar'>
-            <a href='#warme'>Home</a>
-            <a href='#desc'>Description</a>
-            <a href='#calc'>Calculator</a>
-            <a href='#contact'>Contact</a>
-        </div>
-    );
-}
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY < lastScrollY) {
+      setShowNavbar(true); 
+    } else {
+      setShowNavbar(false); 
+    }
+    setLastScrollY(window.scrollY); 
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <div className={`nav-bar ${showNavbar ? 'visible' : 'hidden'}`}>
+      <a href='#warme'>Home</a>
+      <a href='#desc'>Description</a>
+      <a href='#calc'>Calculator</a>
+      <a href='#contact'>Contact</a>
+    </div>
+  );
+};
 
 export default NavBar;
