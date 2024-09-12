63a08a23d5d2ef8dc61cc310d19e17c5a5e02ce9
vctrubio <vctrubio@gmail.com>
2024-09-12 17:02:01 +0200

    calc margins

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,6 +24,33 @@ const NavBar = () => {
     };
   }, [lastScrollY]);
 
+  const debounce = (func, wait) => {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+  };
+
+  const handleLinkClick = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      const offset = 100;
+      const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+
+      debounce(() => {
+        setShowNavbar(false);
+      }, 300)();
+    }
+  };
+
   return (
     <div className={`nav-bar ${showNavbar ? 'visible' : 'hidden'}`}>
       <div className='nav-bar-header'>
@@ -33,10 +60,10 @@ const NavBar = () => {
         <div className='nav-bar-logo'>
           <img src={logoWarme} alt='warme' />
         </div>
-        <a className="nav-bar-mob" href='#warme'>Acerca</a>
-        <a className="nav-bar-mob" href='#desc'>Producto</a>
-        <a className="nav-bar-mob" href='#calc'>Calculador</a>
-        <a className="nav-bar-mob" href='#contact'>Contacto</a>
+        <a className="nav-bar-mob" href='#warme' onClick={(e) => handleLinkClick(e, 'warme')}>Acerca</a>
+        <a className="nav-bar-mob" href='#desc' onClick={(e) => handleLinkClick(e, 'desc')}>Producto</a>
+        <a className="nav-bar-mob" href='#calc' onClick={(e) => handleLinkClick(e, 'calc')}>Calculador</a>
+        <a className="nav-bar-mob" href='#contact' onClick={(e) => handleLinkClick(e, 'contact')}>Contacto</a>
       </div>
     </div>
   );
